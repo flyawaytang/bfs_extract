437258d787d7a628ece32c9b06a0b08db63d8fcd
x+y+z=0 <4126965+flyawaytang@users.noreply.github.com>
2021-08-19 09:32:42 +0800

    Update bfs_extract.sh

--- bfs_extract.sh
+++ bfs_extract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ~/tools/bfs_extract/yank.py $1 stage1-$1
-python3 ~/tools/bfs_extract/bfs.py stage1-$1 stage2-$1
-python3 ~/tools/bfs_extract/bfs_extract.py stage2-$1 $2
+python3 ./yank.py $1 stage1-$1
+python3 ./bfs.py stage1-$1 stage2-$1
+python3 ./bfs_extract.py stage2-$1 $2
